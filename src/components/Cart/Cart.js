4fff3bdb73efa51ed87955c88ac801a8320fd5f0
withnahidul <nahidulislam6800@gmail.com>
2022-03-26 20:26:27 +0600

    responsive

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,16 +4,25 @@ import "./Cart.css";
 const Cart = (props) => {
   const { cart } = props;
   let total = 0;
+  let selectCart='';
 
   for (const product of cart) {
     total = total + product.price;
+    selectCart = selectCart +product.name;
   }
   return (
     <div className="cart">
       <h3>this is for cart</h3>
-      <p>Selected item: {cart.length} </p>
+      <p>select:{cart.length} </p>
+      <p>{selectCart} </p>
       <p>Total price:${total}</p>
+      <div>
+        <button>Coose One</button>
+        <button>Reset</button>
+      </div>
+
     </div>
+
   );
 };
 export default Cart;
