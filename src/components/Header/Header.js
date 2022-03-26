6637b00baf5f2d54399e0f111ccf61287f2c9b06
withnahidul <nahidulislam6800@gmail.com>
2022-03-26 21:11:55 +0600

    header-correction

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <nav className="header">
-      <h2>NiS Beggit Store</h2>
+      <h2>NiS Bag Store</h2>
       <div>
         <a href="/home">Home</a>
         <a href="/order">Order</a>
