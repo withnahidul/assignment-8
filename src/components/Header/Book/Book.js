342fbe28b2618aaca814894408027ed8c848fe27
withnahidul <nahidulislam6800@gmail.com>
2022-03-26 10:16:33 +0600

    data-load

--- src/components/Header/Book/Book.js
+++ src/components/Header/Book/Book.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Book.css';
 
 const Book = () => {
+   const [data, setData] = useState([]);
+   
+   useEffect ( () => {
+fetch ('data.json')
+.then(res=> res.json())
+.then(data => setData(data))
+
+   }, [])
     return (
 <div className='book-container'>
  <div className="prouducts-container">
-<h2>This is for book</h2>
+<h2>This is for book: {data.length}</h2>
  </div>
  <div className="cart-container">
 <h3>this is for cart</h3>
