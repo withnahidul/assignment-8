8af816586ec9146b9df473eff4c4d546a62d8ceb
withnahidul <nahidulislam6800@gmail.com>
2022-03-26 15:30:37 +0600

    icone-add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,16 @@
 import React from "react";
-import './Header.css';
+import "./Header.css";
 
-const Header = () =>{
-    return (
-<nav className= 'header'>
-<h2>NiS Book Store</h2>
-<div>
-    <a href="/home">Home</a>
-    <a href="/order">Order</a>
-    <a href="/about">About</a>
-</div>
-</nav>
-
-    );
-
+const Header = () => {
+  return (
+    <nav className="header">
+      <h2>NiS Beggit Store</h2>
+      <div>
+        <a href="/home">Home</a>
+        <a href="/order">Order</a>
+        <a href="/about">About</a>
+      </div>
+    </nav>
+  );
 };
 export default Header;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
@@ -14,7 +16,9 @@ const Product = (props) => {
          <p>Price:${price}</p>
          </div>
          <button onClick={() =>handleAddtoCart(product)} className='btn-cart'>
-             <p>Add to Cart</p>
+             <p className='btn-text'>Add to Cart</p>
+             <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+
          </button>
         </div>
     );
